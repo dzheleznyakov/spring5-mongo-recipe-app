1516a817ef17567b82253529a255f313a5fe9e14
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-08-28 08:22:58 +0100

    Correcting application defects under MongoDB

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -7,16 +7,16 @@ import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 /**
  * Created by jt on 6/13/17.
  */
 @Getter
 @Setter
-@Document
 public class Ingredient {
     @Id
-    private String id;
+    private String id = UUID.randomUUID().toString();
     private String description;
     private BigDecimal amount;
 
