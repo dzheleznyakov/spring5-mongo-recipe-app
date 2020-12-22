1ad6c113936060d9acd9d1197ba10144b220fc74
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2020-12-22 23:14:36 +0000

    Refactor image service to use reactive types

--- src/main/java/guru/springframework/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/services/ImageServiceImpl.java
@@ -1,11 +1,11 @@
 package guru.springframework.services;
 
 import guru.springframework.domain.Recipe;
-import guru.springframework.repositories.RecipeRepository;
+import guru.springframework.repositories.reactive.RecipeReactiveRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 
@@ -15,21 +15,16 @@ import java.io.IOException;
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+    private final RecipeReactiveRepository recipeRepository;
 
-
-    private final RecipeRepository recipeRepository;
-
-    public ImageServiceImpl( RecipeRepository recipeService) {
-
+    public ImageServiceImpl(RecipeReactiveRepository recipeService) {
         this.recipeRepository = recipeService;
     }
 
     @Override
-    @Transactional
-    public void saveImageFile(String recipeId, MultipartFile file) {
-
+    public Mono<Void> saveImageFile(String recipeId, MultipartFile file) {
         try {
-            Recipe recipe = recipeRepository.findById(recipeId).get();
+            Recipe recipe = recipeRepository.findById(recipeId).block();
 
             Byte[] byteObjects = new Byte[file.getBytes().length];
 
@@ -41,12 +36,14 @@ public class ImageServiceImpl implements ImageService {
 
             recipe.setImage(byteObjects);
 
-            recipeRepository.save(recipe);
+            recipeRepository.save(recipe).block();
+            return Mono.empty();
         } catch (IOException e) {
             //todo handle better
             log.error("Error occurred", e);
 
             e.printStackTrace();
         }
+        return null;
     }
 }
